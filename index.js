c7ee021a66e18cee79acac400769805f5b213db4
Olle Andreasson <oandreasson@dyn3-030.alien.c-base.org>
2017-08-29 20:55:13 +0200

    some stuff

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ var Fs = require('fs');
 var Emitter = require('./lib/dataemitter');
 
 var queue = [
-    "http://www.reddit.com/"
+    "https://c-base.org/"
 ];
 
 var internals = {
     'batch' : 5,
     'interval' : 1000,
     'visit_thresh' : 5,
+    'skip' : ['wiki', 'youtube', 'linkedin', 'google'],
     'targets' : [
         {'path' : 'title'}
         // {'path' : 'a[title]', 'attr' : 'title'}
@@ -50,7 +51,13 @@ internals.handleConsumedResult = (target, res, done) => {
     var emails = res.body.match(/[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.(?!png|jpg|gif)[A-Za-z]{2,4}/g);
 
     $('a[href^=http]').each(function(i, el) {
-        potTargets.push($(this).attr('href').toString());
+      let targetA = $(this).attr('href').toString();
+
+      const shouldNotAdd = internals.skip.reduce((bool, skip) => {
+        return bool || targetA.indexOf(skip) > -1},
+      false);
+
+      if (!shouldNotAdd) potTargets.push($(this).attr('href').toString());
     });
 
     //find the targets in the internals.target css selector
